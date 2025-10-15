ab42bf54b56c052cc0081e837fcb55faa9cbaecd
Christian Surlykke <christian@surlykke.dk>
2025-10-15 16:40:21 +0200

    Some dot-file updates

--- home/config/fish/local_config.fish
+++ home/config/fish/local_config.fish
@@ -1,6 +1,7 @@
 # stuff specific to this machine
 set -gx TERMINAL alacritty
 #set -gx RUN_SWAYIDLE no
-set -gx REFUDE_SWITCHES "--ignore-window Google-chrome --ignore-window Brave-browser --ignore-window Microsoft-edge"
+#set -gx REFUDE_SWITCHES "--ignore-window google-chrome --ignore-window Brave-browser --ignore-window Microsoft-edge"
 #set -gx REFUDE_SWITCHES "--ignore-window google-chrome"
+set -gx REFUDE_SWITCHES "--no-notifications"
 
