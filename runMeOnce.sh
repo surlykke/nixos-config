a7bb185fb63ef3c4c2c4cb86fd4f71fdab00deb1
Christian Surlykke <christian@surlykke.dk>
2025-10-20 14:00:10 +0200

    Pull refude and windowarranger from git in runmeonce.sh

--- runMeOnce.sh
+++ runMeOnce.sh
@@ -15,11 +15,28 @@ $(grep boot.loader /etc/nixos/configuration.nix)
 }
 EOF
 
-sudo mv /etc/nixos /etc/nixos-backup-of-initial
+if ls $HOME/projekter/refude >/dev/null 2>/dev/null; then
+	echo $HOME/projekter/refude is in the way. Please move it.
+fi 
+
+if ls $HOME/projekter/windowarranger >/dev/null 2>/dev/null; then
+	echo $HOME/projekter/windowarranger is in the way. Please move it.
+fi 
+
+mkdir -p $HOME/projekter
+
+pushd $HOME/projekter
+
+git clone https://github.com/surlykke/refude
+git clone https://github.com/surlykke/windowarranger
+
+popd 
 
 cat <<-EOF
 	Setup done.
 	You can now run:
 
 		sudo nixos-rebuild switch -I nixos-config=$(pwd)/configuration.nix
+
+	/etc/nixos left in place, but should not be used
 EOF
