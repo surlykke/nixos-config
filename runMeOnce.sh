97fe4d7f4f8213b21a0bb74ffb693540881e1285
Christian Surlykke <christian@surlykke.dk>
2025-10-21 10:36:09 +0200

    Add info on adding pgks-unstable to runMeOnce.sh

--- runMeOnce.sh
+++ runMeOnce.sh
@@ -30,6 +30,10 @@ cat <<-EOF
 	Setup done.
 	You can now run:
 
+		sudo nix-channel --add https://nixos.org/channels/nixpkgs-unstable pkgs-unstable
+
+	and then:
+
 		sudo nixos-rebuild switch -I nixos-config=$(pwd)/configuration.nix
 
 	/etc/nixos left in place, but should not be used
